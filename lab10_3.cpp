d96f8b65e365f0d9fc4a68ad02f3de6672744667
Peem <mepeem2007@gmail.com>
2025-12-26 19:06:00 +0700

    final

--- lab10_3.cpp
+++ lab10_3.cpp
@@ -1,3 +1,31 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+int main(){
+
+    int count = 0;
+    float sum = 0.0;
+    float sum_of_square = 0.0;
+
+    string textline;
+
+    ifstream source("score.txt");
+
+    while (getline( source, textline)){
+        float x = stof(textline);
+        sum += x;
+        sum_of_square += x*x;
+        count++;
+    }
+
+        cout << "Number of data = " << count << "\n";
+        cout << setprecision(3);
+        cout << "Mean = " << sum/count << "\n";
+        cout << "Standard deviation = " << sqrt((sum_of_square/count) - pow( sum/count, 2));
+
+}
